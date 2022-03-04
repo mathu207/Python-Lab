318d6b727462c2b94a72997440f267b2f56cfe12
Mathu S Kulathumkal <94525554+mathu207@users.noreply.github.com>
2022-03-04 18:15:32 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+#Anu Sunny
+#21MCA_006
+
+import calc
+
+#print(calc.add(10, 2))
+n1=int(input('Enter first numbers'))
+n2=int(input('Enter first numbers'))
+
+print('addition :',calc.add(n1,n2))
+print('subtraction :',calc.sub(n1,n2))
+print('multiplication :',calc.mul(n1,n2))
+print('division :',calc.div(n1,n2))
+
+
+
